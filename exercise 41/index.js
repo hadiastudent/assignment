30cf314be5bf274d3c5c56604d2a49f992228914
Hadia <141926293+hadiastudent@users.noreply.github.com>
2023-09-29 02:48:23 +0500

    Add files via upload

---
+++ exercise 41/index.js
@@ -0,0 +1,11 @@
+"use strict";
+//#41
+/*. Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(), which prints the name of each magician in the array.
+*/
+let magicians = ["Harry Houdini", "David Copperfield", "Penn Jillette", "Teller"];
+function show_magicians(magicians) {
+    for (let magician of magicians) {
+        console.log(magician);
+    }
+}
+show_magicians(magicians);
