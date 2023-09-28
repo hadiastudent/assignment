f9630dd7b74506b32fb8c235f8c463518df5360b
Hadia <141926293+hadiastudent@users.noreply.github.com>
2023-09-29 02:49:17 +0500

    Add files via upload

---
+++ exercise 43/index.js
@@ -0,0 +1,22 @@
+"use strict";
+//#43
+/*. Unchanged Magicians: Start with your work from Exercise 40. Call the function make_great() with a copy of the array of magicians’ names. Because the original array will be unchanged, return the new array and store it in a separate array. Call show_magicians() with each array to show that you have one array of the original names and one array with the Great added to each magician’s name.
+*/
+let magiciansNames = ["Harry Houdini", "David Copperfield", "Penn Jillette", "Teller"];
+function show_magicians(magicians) {
+    for (let magician of magicians) {
+        console.log(magician);
+    }
+}
+function make_great(magicians) {
+    let great_magicians = magicians.map(magician => `the Great ${magician}`);
+    return great_magicians;
+}
+//modify the array to add `the Great` 
+let great_magiciansNames = make_great(magiciansNames);
+//call the function to show the original magicians names
+console.log("Original Magicians:");
+show_magicians(magiciansNames);
+//call the function to show the great magicians names
+console.log("\nGreat Magicians:");
+show_magicians(great_magiciansNames);
