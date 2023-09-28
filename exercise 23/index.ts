5fd588fe7359f056b42be8540944c1f549aa5d4f
Hadia <141926293+hadiastudent@users.noreply.github.com>
2023-09-29 02:41:55 +0500

    Add files via upload

---
+++ exercise 23/index.ts
@@ -0,0 +1,44 @@
+//#23
+/* Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+
+let car = 'subaru';
+
+console.log("Is car == 'subaru'? I predict True.")
+
+console.log(car == 'subaru')
+
+• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.*/
+
+let car= `subaru`
+
+console.log("Is car ==`subaru`? I predict true.")
+console.log(car==`subaru`)
+
+console.log("Is car ==`civiv`? I predict false.")
+console.log(car==`civic`)
+
+console.log("Is car!==`subaru`? I predict false.")
+console.log(car!==`subaru`)
+
+console.log("Is car!==`Honda`? I predict true.")
+console.log(car!==`Honda`)
+
+console.log("Is car==`Rivo`? I predict false.")
+console.log(car==`Rivo`)
+
+console.log("Is car!==`Bike`? I predict true.")
+console.log(car!==`Bike`)
+
+console.log("Is car==`Cycle`? I predict false.")
+console.log(car==`Cycle`)
+
+console.log("Is car!==`Bus`? I predict true. ")
+console.log(car!==`Bus`)
+
+console.log("Is car!==`Boat`? I predict true.")
+console.log(car!==`Boat`)
+
+console.log("Is car==`Van`? I predict false.")
+console.log(car==`Van`)
