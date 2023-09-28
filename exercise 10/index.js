324ff92345d2bb650b22753f6dcba5b0a3ed575e
Hadia <141926293+hadiastudent@users.noreply.github.com>
2023-09-29 02:32:16 +0500

    Add files via upload

---
+++ exercise 10/index.js
@@ -0,0 +1,12 @@
+"use strict";
+//#10
+//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+Object.defineProperty(exports, "__esModule", { value: true });
+//in this code i have saved my favourite number
+let favourite_Number = 6;
+let message = `My favourite no is ${favourite_Number}`;
+console.log(message);
+//i have saved person name with a special message ,so that wherever needed i can use it.
+let personName = "Anaya";
+let message1 = `Hello ${personName}, would you like to learn python today?`;
+console.log(message1);
---
+++ exercise 10/index.ts
@@ -0,0 +1,16 @@
+//#10
+//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+
+//in this code i have saved my favourite number
+let favourite_Number: number= 6
+let message : string= `My favourite no is ${favourite_Number}`
+console.log (message);
+
+
+//i have saved person name with a special message ,so that wherever needed i can use it.
+let personName: string = "Anaya"
+let message1 : string = `Hello ${personName}, would you like to learn python today?`
+console.log(message1)
+
+export{}
