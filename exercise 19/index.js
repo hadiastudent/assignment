c72053f652f31ee7b9adadaf936d683655a59b51
Hadia <141926293+hadiastudent@users.noreply.github.com>
2023-09-29 02:38:18 +0500

    Add files via upload

---
+++ exercise 19/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//#19
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+const Exercise_15_1 = require("../Exercise 15");
+console.log(`\nPrint a message to show no of guests invited.`);
+console.log(`We have finally invited ${Exercise_15_1.guest_list.length} to dinner.`);
---
+++ exercise 19/index.ts
@@ -0,0 +1,5 @@
+//#19
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+import{ guest_list } from '../Exercise 15'
+console.log(`\nPrint a message to show no of guests invited.`)
+console.log(`We have finally invited ${guest_list.length} to dinner.`)
