6ccbc5b9be7f120054ed912d8ed6f5fbcba6220c
Hadia <141926293+hadiastudent@users.noreply.github.com>
2023-09-29 02:31:34 +0500

    Add files via upload

---
+++ exercise 9/index.js
@@ -0,0 +1,6 @@
+"use strict";
+//#9
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+let favourite_Number = 6;
+let message = `My favourite no is ${favourite_Number}`;
+console.log(message);
