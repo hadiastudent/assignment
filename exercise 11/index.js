ce863c92684945f4abe6b7086a6a5f974656c6aa
Hadia <141926293+hadiastudent@users.noreply.github.com>
2023-09-29 02:32:42 +0500

    Add files via upload

---
+++ exercise 11/index.js
@@ -0,0 +1,7 @@
+"use strict";
+//#12
+//Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+let Names = [`Fatima`, `Anaya`, `Fauzia`, `Sahar`, `Talha`];
+for (let Name of Names) {
+    console.log(`Hello ${Name}, good morning`);
+}
