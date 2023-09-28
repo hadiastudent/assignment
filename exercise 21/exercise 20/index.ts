18195f52366784a0a589a09f4c5f4a32374ca165
Hadia <141926293+hadiastudent@users.noreply.github.com>
2023-09-29 02:40:56 +0500

    Add files via upload

---
+++ exercise 21/exercise 20/index.ts
@@ -0,0 +1,8 @@
+//#20
+//Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+let citiesList: string[]= ["Lahore", "Islamabad", "Karachi", "Peshawar","Faisalabad"] 
+console.log(`list of cities :`)
+for(let city of citiesList){(
+    console.log(city))
+}
---
+++ exercise 21/index.js
@@ -0,0 +1,33 @@
+"use strict";
+//#21
+//. They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+let bouquet = { name: `Red rose`,
+    price: 500,
+    description: `beautiful arrangement of red rose.` };
+// define an array of objects
+let bouquets = [];
+//storing objects in array
+bouquets.push(bouquet);
+// object 2
+let bouquet1 = {
+    name: `summer bliss`,
+    price: 200,
+    description: `beautiful arrangement of summer bliss.`
+};
+bouquets.push(bouquet1);
+//object 3
+let bouquet2 = {
+    name: `Red hot`,
+    price: 600,
+    description: `beautiful arrangement of red hot flowers.`
+};
+bouquets.push(bouquet2);
+console.log(bouquets);
+//function to display array of bouquet
+function displaybouquets(bouquets) {
+    for (let i of bouquets) {
+        console.log(`title : ${i.name}`, `price:${i.price}`, `description:${i.description}`, `\u2014,
+     \n`);
+    }
+}
+displaybouquets(bouquets);
